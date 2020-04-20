53ce631c77f8bb32e1c25da173cf137cf7b49d60
Prashanth N <prashanth.t.r.n@gmail.com>
2020-04-20 15:31:57 -0700

    added database connection and models

---
+++ backend/models/seller.js
@@ -0,0 +1,25 @@
+'use strict';
+var mongoose = require('mongoose');
+
+const SellerSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.SchemaTypes.ObjectId,
+    required: true,
+    ref: 'User'
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  profilePicture: {
+    type: String
+  },
+  addresses: {
+    type: String
+  },
+},
+{
+  timestamps: true
+});
+
+module.exports = mongoose.model.Seller || mongoose.model('Seller', SellerSchema);
