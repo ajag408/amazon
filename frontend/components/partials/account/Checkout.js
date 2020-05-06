1a56d7e5ea1f01ded492b5935484ce2fce44e17b
Akash Jag <akashjagannathan408@gmail.com>
2020-05-06 16:37:06 -0700

    started linting frontend components

--- frontend/components/partials/account/Checkout.js
+++ frontend/components/partials/account/Checkout.js
@@ -2,93 +2,83 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Router from 'next/router';
 import FormCheckoutInformation from './modules/FormCheckoutInformation';
-import {backendurl} from './../../../backendurl';
-import { Form, Input, Select} from 'antd';
-import Link from 'next/link';
-const {Option} = Select;
+import { backendurl } from '../../../backendurl';
+
 
 class Checkout extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            storage: '',
-            cartTotal: '',
-            cartItems: [],
+  constructor(props) {
+    super(props);
+    this.state = {
+      storage: '',
+      cartTotal: '',
+      cartItems: [],
 
-        };
-    }
+    };
+  }
 
 
-    componentDidMount(){
-        this.setState({ 
-            storage : localStorage
-        }, () => {
-            console.log("hello")
-            
-            const {storage} = this.state;
-            // alert(storage.role);
-            // console.log(storage);
-            if(!storage.token || storage.role != "Customer"){
-                
-                Router.push('/account/login')
+  componentDidMount() {
+    this.setState({
+      storage: localStorage,
+    }, () => {
+      console.log('hello');
+
+      const { storage } = this.state;
+      // alert(storage.role);
+      // console.log(storage);
+      if (!storage.token || storage.role !== 'Customer') {
+        Router.push('/account/login');
+      } else {
+        // replace hard-coded with local storage id
+
+        // axios.get(`${backendurl}/customer/getCustomer/5ea32fb4716ebc4f57fd8ae9`)
+        // .then((res) => {
+        //     // console.log("customer address",res.data[0].savedAddresses);
+        //     this.setState({
+        //         savedAddresses: res.data[0].savedAddresses
+        //     })
+
+        // });
+
+        axios.get(`${backendurl}/cart/customer/${storage.user_id}/show-cart`)
+          .then((res) => {
+            console.log(res);
+            if (res.data.cartItems.length === 0) {
+              alert('Cart is empty! Go shop!');
+              Router.push('/');
             } else {
-                //replace hard-coded with local storage id
+              this.setState({
+                cartTotal: res.data.cartTotal,
+                cartItems: res.data.cartItems,
 
-                // axios.get(`${backendurl}/customer/getCustomer/5ea32fb4716ebc4f57fd8ae9`)
-                // .then((res) => {
-                //     // console.log("customer address",res.data[0].savedAddresses);
-                //     this.setState({
-                //         savedAddresses: res.data[0].savedAddresses
-                //     })
-    
-                // });
-
-                axios.get(`${backendurl}/cart/customer/` + storage.user_id + `/show-cart`)
-                .then((res) => {
-                    console.log(res);
-                    if(res.data.cartItems.length == 0){
-                        alert("Cart is empty! Go shop!")
-                        Router.push('/')
-                    } else {
-                    this.setState({
-                        cartTotal: res.data.cartTotal,
-                        cartItems: res.data.cartItems,
-
-                    })
-                }
-        
-    
-                });
-        
+              });
             }
-        });
-    }
-    
-        
+          });
+      }
+    });
+  }
 
-        render() {
-            const { cartTotal, cartItems } = this.state;
-            return (
-                <div className="ps-checkout ps-section--shopping">
-                    <div className="container">
-                        <div className="ps-section__header">
-                            <h1>Checkout Information</h1>
-                        </div>
-                        <div className="ps-section__content">
-                            <FormCheckoutInformation
+
+  render() {
+    const { cartTotal, cartItems } = this.state;
+    return (
+      <div className="ps-checkout ps-section--shopping">
+        <div className="container">
+          <div className="ps-section__header">
+            <h1>Checkout Information</h1>
+          </div>
+          <div className="ps-section__content">
+            <FormCheckoutInformation
                                 // amount={amount}
-                                cartTotal={cartTotal}
-                                cartItems={cartItems}
-                            />
-                        </div>
-                    </div>
-                </div>
-            );
-        }
-    }
-    
-    
-    export default Checkout;
-    
+              cartTotal={cartTotal}
+              cartItems={cartItems}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 
+export default Checkout;
--- frontend/pages/account/order-summary.js
+++ frontend/pages/account/order-summary.js
@@ -4,35 +4,36 @@ import BreadCrumb from '../../components/elements/BreadCrumb';
 import HeaderMobile from '../../components/shared/headers/HeaderMobile';
 import NavigationList from '../../components/shared/navigation/NavigationList';
 import OrderSummary from '../../components/partials/account/OrderSummary';
+
 const PaymentPage = () => {
-    const breadCrumb = [
-        {
-            text: 'Home',
-            url: '/',
-        },
-        {
-            text: 'Shopping Cart',
-            url: '/account/shopping-cart',
-        },
-        {
-            text: 'Checkout Information',
-            url: '/account/checkout',
-        },
-        {
-            text: 'Payment',
-        },
-    ];
-    return (
-        <div className="site-content">
-            <HeaderDefault />
-            <HeaderMobile />
-            <NavigationList />
-            <div className="ps-page--simple">
-                <BreadCrumb breacrumb={breadCrumb} />
-                <OrderSummary />
-            </div>
-        </div>
-    );
+  const breadCrumb = [
+    {
+      text: 'Home',
+      url: '/',
+    },
+    {
+      text: 'Shopping Cart',
+      url: '/account/shopping-cart',
+    },
+    {
+      text: 'Checkout Information',
+      url: '/account/checkout',
+    },
+    {
+      text: 'Payment',
+    },
+  ];
+  return (
+    <div className="site-content">
+      <HeaderDefault />
+      <HeaderMobile />
+      <NavigationList />
+      <div className="ps-page--simple">
+        <BreadCrumb breacrumb={breadCrumb} />
+        <OrderSummary />
+      </div>
+    </div>
+  );
 };
 
 export default PaymentPage;
