50f55a707d5c045fb9b5636e3eab125ebf240f18
Prashanth N <prashanth.t.r.n@gmail.com>
2020-05-08 01:39:13 -0700

    add to cart and rating no access to seller

--- frontend/components/elements/detail/modules/description/DefaultDescription.js
+++ frontend/components/elements/detail/modules/description/DefaultDescription.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
 import { Tabs } from 'antd';
-const { TabPane } = Tabs;
 
 import PartialDescription from './PartialDescription';
 import PartialSpecification from './PartialSpecification';
@@ -9,31 +8,42 @@ import PartialVendor from './PartialVendor';
 import PartialReview from './PartialReview';
 import PartialOffer from './PartialOffer';
 
+const { TabPane } = Tabs;
+
 class DefaultDescription extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
+    this.state = {
+      role: ''
     }
+  }
 
-    componentWillReceiveProps(){
-        //console.log("DefaultDescription componentWillReceiveProps props are: ", this.props);
-    }
-    render() {
+  componentDidMount(){
+    this.setState({
+      role: localStorage.getItem('role')
+    })
+  }
 
-        return (
-            <div>
-                <div className="ps-product__content ps-tab-root">
-                    <Tabs defaultActiveKey="1">
-                        <TabPane tab="Description" key="1">
-                            <PartialDescription product={this.props.product}/>
-                        </TabPane>
-                        <TabPane tab={`Reviews (${this.props.product.ratingAndReviews.length})`} key="4">
-                            <PartialReview product={this.props.product} />
-                        </TabPane>
-                    </Tabs>
-                </div>
-            </div>
-        );
-    }
+  componentWillReceiveProps() {
+    // console.log("DefaultDescription componentWillReceiveProps props are: ", this.props);
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="ps-product__content ps-tab-root">
+          <Tabs defaultActiveKey="1">
+            <TabPane tab="Description" key="1">
+              <PartialDescription product={this.props.product} />
+            </TabPane>
+            {this.state.role === 'Customer' ? (<TabPane tab={`Reviews (${this.props.product.ratingAndReviews.length})`} key="4">
+              <PartialReview product={this.props.product} />
+            </TabPane>) : (null)}
+          </Tabs>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default DefaultDescription;
